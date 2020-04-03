7dd395de6f73f9744e074ce02dbc0c9399ca6b13
mkhlamovHS <maxim.khlamov@hotelshopping.ru>
2020-04-03 12:36:18 +0300

    Add test scene and shooting script

---
+++ Assets/Scrips/Camera/SimpleMouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float Sensitivity = 1f;
+    
+    private float _x;
+    private float _y;
+    const float MIN_X = 0.0f;
+    const float MAX_X = 360.0f;
+    const float MIN_Y = -90.0f;
+    const float MAX_Y = 90.0f;
+ 
+    
+ 
+    void Awake()
+    {
+        var euler = transform.rotation.eulerAngles;
+        _x = euler.x;
+        _y = euler.y;
+    }
+ 
+    void Update()
+    {
+        
+ 
+        _x -= Input.GetAxis("Mouse X") * (Sensitivity * Time.deltaTime);
+        if (_x < MIN_X) _x += MAX_X;
+        else if (_x > MAX_X) _x -= MAX_X;
+        _y += Input.GetAxis("Mouse Y") * (Sensitivity * Time.deltaTime);
+        if (_y < MIN_Y) _y = MIN_Y;
+        else if (_y > MAX_Y) _y = MAX_Y;
+ 
+        transform.rotation = Quaternion.Euler(-_y, -_x, 0.0f);
+    }
+}
---
+++ Assets/Scrips/ShooterFromCamera.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace iDoctorTestTask
+{
+    public class ShooterFromCamera : MonoBehaviour
+    {
+        [SerializeField] private Camera arCamera;
+        
+        [SerializeField] private float rayDistanceFromCamera = 10.0f;
+
+        [SerializeField] private float shootingRate = 0.2f;
+        private float shootingTimer = 0f;
+        
+        [SerializeField] private GameObject sight;
+
+        private void Update()
+        {
+            if (shootingTimer >= shootingRate)
+            {
+                // Shoot on tap or click
+#if UNITY_EDITOR
+                if (Input.GetMouseButtonUp(0))
+#else
+                if (Input.touchCount > 0)
+#endif
+                {
+                    var ray = arCamera.ScreenPointToRay(sight.transform.position);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, rayDistanceFromCamera))
+                    {
+                        var enemy = hit.transform;
+                        if (enemy.name == "Enemy")
+                        {
+                            Debug.Log($"Hit {enemy.name} {enemy.transform.position}");
+                        }
+                    }
+
+                    shootingTimer = 0f;
+                }
+            }
+            else
+            {
+                shootingTimer += Time.unscaledDeltaTime;
+            }
+        }
+    }
+}
