9623e7f78095899743ad5555941b7871a27cb819
mkhlamovHS <maxim.khlamov@hotelshopping.ru>
2020-04-04 11:50:32 +0300

    Disable screen dim

---
+++ Assets/Scrips/ScreenSleepTimeout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace iDoctorTestTask
+{
+    public class ScreenSleepTimeout : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Disable screen dimming
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+    }
+}
